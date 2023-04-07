967e4441c97f21655ff5418a42391d081efc667e
JoshuaT34 <102335066+JoshuaT34@users.noreply.github.com>
2023-04-07 07:58:56 -0500

    Update GUIV5.lua

--- GUIV5.lua
+++ GUIV5.lua
@@ -5067,30 +5067,28 @@ end
 FlingTargetButton.MouseButton1Down:Connect(Fling)
 local isSpectating = false
 function Kidnap()
-	game.Players.LocalPlayer:GetMouse().KeyDown:connect(function(key)
-	if key == 'z' then
-		local h = game.Players.LocalPlayer.Character.Humanoid:Clone()
-		local plr = game.Players.LocalPlayer.Character
-		local CF = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-		local lp = game.Players.LocalPlayer
+	Goto();
+	wait()
+	local h = game.Players.LocalPlayer.Character.Humanoid:Clone()
+	local plr = game.Players.LocalPlayer.Character
+	local CF = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+	local lp = game.Players.LocalPlayer
 
-		h.Parent = game.Players.LocalPlayer.Character
-		h.Name = "Hum"
+	h.Parent = game.Players.LocalPlayer.Character
+	h.Name = "Hum"
 
-		game.Players.LocalPlayer.Character.Humanoid:Destroy()
+	game.Players.LocalPlayer.Character.Humanoid:Destroy()
 
-		for _,v in pairs(game.Players.LocalPlayer:GetDescendants()) do
-		    if v.ClassName == "Tool" then
-			v.Parent = plr
-		    end
+	for _,v in pairs(game.Players.LocalPlayer:GetDescendants()) do
+	    if v.ClassName == "Tool" then
+		v.Parent = plr
+	    end
 	end
 	wait(.5)
 	tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(300, Enum.EasingStyle.Linear)
 
 	tween = tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(0, -1000, 0)})
 	tween:Play()
-	end
-	end)
 end
 KidnapButton.MouseButton1Down:Connect(Kidnap)
 --stats
